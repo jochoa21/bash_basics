47c66b292e211f4303e2eb5d2e91add8993f3ed3
Sebastian <ochoasebastian08@gmail.com>
2026-04-08 00:52:19 -0400

    Completed exercise in parallel universe

--- 01-read_input.sh
+++ 01-read_input.sh
@@ -10,3 +10,7 @@ echo 'Your name was stored in $name'
 
 # exercise: write a script that asks the user for a 
 # filename and create an empty file named after it
+echo "Name of new file:"
+read userfile
+echo "# New File" > "$userfile"
+echo "Created $userfile with a header"
